3e89833d223a4cbff26ce629ac10442a8df05f58
Victor Khatuncev <139468165+vibisol@users.noreply.github.com>
2023-11-16 12:54:27 +0100

    n

--- api.js
+++
@@ -1,5 +0,0 @@
-const data = require('../TestTaskPanel/db.json');
-
-module.exports = (req, res) => {
-    res.status(200).json(data);
-};
---
+++ api/products.js
@@ -0,0 +1,5 @@
+import data from '../db.json';
+
+export default (req, res) => {
+    res.status(200).json(data);
+};
